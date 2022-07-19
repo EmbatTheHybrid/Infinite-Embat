c6f526e18daca9aa7223c54b0f0343f1f61b9a65
Embat <grimajenna80@gmail.com>
2022-07-19 19:13:08 +0200

    Added stuff

--- app.js
+++ app.js
@@ -10,7 +10,18 @@ window.onscroll = function() {
 		return
 	}
 
+	// Create image
+
 	last = last.cloneNode(true)
-	last.firstElementChild.src = './images/' + (Math.floor(Math.random() * 13) + 1) + ".png"
+
+	if (Math.floor(Math.random() * 100) <= 20) {
+		// Easter egg
+		last.firstElementChild.src = './images/EasterEgg/' + 
+			Math.floor(Math.random() * 1000) <= 2 ? 'Shitstappen' : (Math.floor(Math.random() * 4) + 1) + 
+			'.png'
+	} else {
+		last.firstElementChild.src = './images/' + (Math.floor(Math.random() * 13) + 1) + ".png"
+	}
+
 	parent.appendChild(last)
 }
